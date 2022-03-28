e62c011ee74d0649a6b628648a3326e348241037
Ramon Paulo Antonio Caumban <89231021+ramonPL14@users.noreply.github.com>
2022-03-28 10:40:46 +0800

    Add files via upload

---
+++ 2d array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int profile(int num[3][2],int id, int pin){
+    int flag = 0;
+    for(int i = 0; i<3; i++){
+        if(id == num[i][0] && pin == num[i][1]){
+            flag = 1;
+        }
+    }
+    return flag;
+}
+int main(){
+    int num[3][2] = {{1234,0000},{2345,0000},{3456,0000}};
+    int id,pin;
+    printf("Enter ID Number: ");
+    scanf("%d",&id);
+    printf("Enter PIN: ");
+    scanf("%d",&pin);
+    if(profile(num,id,pin)){
+        printf("You have successfully logged in\nID#:%d",id);
+    } else {
+        printf("Invalid ID/PIN!");
+    }
+    return 0;
+}
