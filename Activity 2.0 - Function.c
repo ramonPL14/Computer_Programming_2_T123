0aa07c609a8e9299b62e1afd87c449bbbafd8812
Ramon Paulo Antonio Caumban <89231021+ramonPL14@users.noreply.github.com>
2022-07-15 20:57:21 +0800

    Add files via upload

--- Activity 2.0 - Function.c
+++ Activity 2.0 - Function.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
-int addGrades(int s1, int s2, int s3, int s4, int s5)
-{
-  int sum;
-  sum = s1 + s2 + s3 + s4 + s5;
-  return sum;
+int addGrades(int s1, int s2, int s3, int s4, int s5) {
+      int sum;
+      sum = s1 + s2 + s3 + s4 + s5;
+     
+    return sum;
 }
-int main() {
-  int math, science, english, filipino, history, avg;
-  printf("Enter your grade in Math: ");
-  scanf("%d", &math);
-  printf("Enter your grade in Science: ");
-  scanf("%d", &science);
-  printf("Enter your grade in English: ");
-  scanf("%d", &english);
-  printf("Enter your grade in Filipino: ");
-  scanf("%d", &filipino);
-  printf("Enter your grade in Histroy: ");
-  scanf("%d", &history);
-  avg = addGrades(math, science, english, filipino, history);
-  avg /= 5;
-  printf("\nAVERAGE = %d\n", avg);
-  if(avg > 0)
-  printf("\nGreat job!");
-  printf(" Plus Ultra!");
-  return 0;
-}
-    
+int main()
+{    
+    int math, science, english, filipino, history, avg;
+     
+    printf("Enter your grade in Math: ");
+      scanf("%d", &math);
+      printf("Enter your grade in Science: ");
+     
+    scanf("%d", &science);
+     
+    printf("Enter your grade in English: ");
+      scanf("%d", &english);
+     
+    printf("Enter your grade in Filipino: ");
+      scanf("%d", &filipino);
+     
+    printf("Enter your grade in Histroy: ");
+      scanf("%d", &history);
+      avg = addGrades(math, science, english, filipino, history);
+      avg /= 5;
+      printf("\nAVERAGE = %d\n", avg);
+      if(avg > 0) 
+      printf("\nGreat job!");
+      printf(" Plus Ultra!");
+      return 0;
+}    
